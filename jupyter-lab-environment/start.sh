5a18a141b61f82b8222db7ef493a786365f875d0
BenSchZA <bscholtz.bds@gmail.com>
2019-07-17 12:09:31 +0200

    Configure ports and access

--- jupyter-lab-environment/start.sh
+++ jupyter-lab-environment/start.sh
@@ -3,4 +3,4 @@
 # Step 2:
 export CACHIX_SIGNING_KEY='4igT/1n2QYaT2doKGL0KzNcXGUPVpZOJcxnqRGCmhxWSnvon5Q2G1mIgWT91+G/B6dpbPck8dx0/QuTCdH6w2A=='
 cachix use linum-jupyter-lab
-nix-shell --command "jupyter lab --no-browser"
+nix-shell --command "jupyter lab --allow-root --no-browser --ip=0.0.0.0 --port=8888 --LabApp.token=''"
